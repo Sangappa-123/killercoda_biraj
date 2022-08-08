f8e491d655d0069f9600ea133f8aec4148ed794d
sakhaglobal <birajsd@gmail.com>
2022-08-08 10:46:07 +0530

    new

--- docker-copy/step1/verify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker exec -it docker stat ~/
-
-# docker cp mycontainer:/home/application.py /root/filesystem/
-# cat /filesystem/application.py
-# docker exec -it docker stat /home/application.py
-# if docker exec -it docker stat /home/application.py; then exit 1; fi
-# if docker ps | grep "new-docker"; then exit 1; fi
